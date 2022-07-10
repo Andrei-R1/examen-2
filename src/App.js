320661e46f8125e5dd828eacad75880a0d84e34e
Andrei Rivera <andreirive2003@gmail.com>
2022-07-09 18:16:20 -0600

    Boton Get Random Hero con solicitud al API y un poquito de Bootstrap en los archivos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import CharacterList from './components/CharacterList';
 import Character from './components/Character';
-
 
 function App() {
   
   return (
-    <div>
-      <h1>Super Hero API</h1>
-      <CharacterList></CharacterList>
+    <div className='bg-dark text-white'>
       <Character></Character>
     </div>
   )
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,32 +1,58 @@
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 
+function RndCharacter (props) {
+  return (
+    <header className='d-flex justify-content-between align-items-center'>
+      <h1 className='text-center py-4'>My Hero List</h1>
+      <button className='btn btn-primary' 
+      onClick={() => props.setId(Math.floor(Math.random() * 732) + 1)}
+      >
+        Get New Random Hero
+      </button>
+    </header>
+  )
+}
+
 function Character() {
-  
+  const [loading, setLoading] = useState(true);
   const [characters, setCharacters] = useState([])
-
+  const randomId = Math.floor(Math.random() * 732) + 1;
+  const [id, setId] = useState(randomId);
+  
   useEffect(() => {
-    async function fetchData() {
-      const {data} = await axios({
-        method: 'get',
-        url: `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/2128939953945151/732`,
-      })
-      setCharacters(data);
-      console.log(data);
+    try {
+      async function fetchData() {
+        const {data} = await axios({
+          method: 'get',
+          url: `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/2128939953945151/${id}`,
+        })
+        setLoading(false);
+        setCharacters(data);
+        console.log(data);
+      }
+      fetchData();
+    } catch (error) {
+      console.log(error);
     }
-    fetchData();
-  }, []);
+  }, [id]);
 
+  if (loading) {
+    return <div>Loading...</div>
+  }
+    
   return (
     <div>
-      {
-        <div>
-          <h2>{characters.name}</h2>
-          <img src={characters.image} alt={characters.name}/>
+      <RndCharacter id={id} setId={setId} />
+      { loading ? (<div>Loading...</div>
+      ):(
+        <div className='text-center p-5'>
+          <h3>{characters.name}</h3>
+          <img src={characters.image.url} alt={characters.name}/>
         </div>
-      }
+      )} 
     </div>
   )
 }
 
-export default Character
+export default Character;
